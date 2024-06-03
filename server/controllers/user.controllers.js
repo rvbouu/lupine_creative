9bd23c9fae2b7cf74afedfa3e91660f5e81c8d3d
Vanessa Bou <rvbouu@gmail.com>
2024-06-03 14:24:17 -0500

    completed user-controller

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -20,45 +20,57 @@ module.exports = {
 
       if(!user){
         return res.status(404).json({message: 'No user found with this ID.'})
-      }
+      };
+      res.status(200).json(user);
     } catch(err){
-      throw new Error(err.message)
-    }
-  },
-
-  async getUserById(req, res){
-    try {
-      return await User.findById(id)
-    } catch(err){
-      throw new Error(err.message)
+      console.log(err)
+      return res.status(500).json(err);
     }
   },
 
   async createUser(req, res){
     try {
-      return await User.create(data)
+      const user = await User.create(req.body);
+
+      return res.status(200).json(user);
     } catch(err){
-      throw new Error(err.message)
+      console.log(err)
+      return res.status(500).json(err);
     }
   },
 
   async updateUserById(req, res){
     try {
-      return await User.findByIdAndUpdate(
-        id, 
-        data, 
-        { new: true }
-      )
+      const user = await User.findByIdAndUpdate(
+        {_id: req.params.userId}, 
+        {$set: req.body}, 
+        { runValidators: true ,new: true }
+      );
+
+      if(!user){
+        return res.status(404).json({message: 'No user found with this ID.'})
+      };
+
+      return res.status(200).json(user)
     } catch(err){
-      throw new Error(err.message)
+      console.log(err)
+      return res.status(500).json(err);
     }
   },
 
   async deleteUserById(req, res){
     try {
-      return await User.findByIdAndDelete(id)
+      const user = await User.findByIdAndDelete({_id: req.params.userId})
+
+      if(!user){
+        return res.status(404).json({message: 'No user found with this ID.'})
+      };
+
+      return res.status(200).json({message: 'User has been deleted.'});
+
     } catch(err){
-      throw new Error(err.message)
+      console.log(err);
+      return res.status(500).json(err);
     }
   }
 
