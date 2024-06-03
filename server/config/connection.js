7d910100d14f6d0b22830ae2527db285735e6f38
Vanessa Bou <rvbouu@gmail.com>
2024-06-03 11:05:03 -0500

    connected db to lupine_creativeDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/dizzyDB';
+const connectionString = 'mongodb://127.0.0.1:27017/lupine_creativeDB';
 
 connect(connectionString);
 
