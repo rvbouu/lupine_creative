a4c693f9e27500be249b88e403e8169e8211e9cc
Vanessa Bou <rvbouu@gmail.com>
2024-06-04 14:48:04 -0500

    researching api

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,1 +1,27 @@
 const router = require('express').Router();
+const Stripe = require('stripe');
+require('dotenv').config();
+
+const stripe = Stripe(process.env.STRIP_KEY);
+
+router.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${process.env.CLIENT_URL}`,
+    cancel_url: 'http://localhost:3001/cancel',
+  });
+
+  res.redirect(303, session.url);
+});
