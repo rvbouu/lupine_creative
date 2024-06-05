983e042fa0f9498aa274da738b00301ab1aed123
Vanessa Bou <rvbouu@gmail.com>
2024-06-04 19:47:44 -0500

    created checkout/stripe backend

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require("./routes");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
