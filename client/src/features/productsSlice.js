abd15fa34a8cc65a38bc8e1d2f8d3d35ec1022a5
Brittany Jungclaus <164208757+NovaLanceBrittany@users.noreply.github.com>
2024-06-06 14:40:59 -0500

    strip provides a decent cart starter code kinda which makes my process a bit obsolete. The folders and Files remain, but commented out with a note.

--- client/src/features/productsSlice.js
+++ client/src/features/productsSlice.js
@@ -1,47 +1,50 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-
-// Video 7 from this series  https://www.youtube.com/watch?v=0T7P6Mb2XNE&list=PL63c_Ws9ecIRnNHCSqmIzfsMAYZrN71L6&index=9
-const initialState = {
-  items: [],
-  status: null,
-  error: null
-};
+// Website changed Direction > Stripe Library housed our Cart Structure, Code is not needed
 
 
-export const productsFetch = createAsyncThunk(
-  "products/productsFetch",
-  async () => {
-    // try {
-
-      const response = await axios.get("http://localhost:5173/shop")
-      return response?.data;
-    // } catch (err) {
-      // return rejectWithValue("An error occured: Features > ProductsSlice.js");
-  }
-);
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
 
 
-// logic on retaining Users and Actions
-const productsSlice = createSlice({
-  name: "products",
-  initialState, 
-  reducers: {},
-  // used to have actions that are already defined
-  extraReducers: {
-    [productsFetch.pending]: (state, action) => {
-      // This ACTS like it mutates the state using redux toolkit 
-      state.status = "pending"
-    },
-    [productsFetch.fulfilled]: (state, action) => {
-      state.status = "success"
-      state.items = action.payload
-    },
-    [productsFetch.rejected]: (state, action) => {
-      state.status = "pending"
-      state.error = action.payload;
-    }
-  }
-})
+// const initialState = {
+//   items: [],
+//   status: null,
+//   error: null
+// };
 
-export default productsSlice.reducer;
+
+// export const productsFetch = createAsyncThunk(
+//   "products/productsFetch",
+//   async () => {
+//     // try {
+
+//       const response = await axios.get("http://localhost:5173/shop")
+//       return response?.data;
+//     // } catch (err) {
+//       // return rejectWithValue("An error occured: Features > ProductsSlice.js");
+//   }
+// );
+
+
+// // logic on retaining Users and Actions
+// const productsSlice = createSlice({
+//   name: "products",
+//   initialState, 
+//   reducers: {},
+//   // used to have actions that are already defined
+//   extraReducers: {
+//     [productsFetch.pending]: (state, action) => {
+//       // This ACTS like it mutates the state using redux toolkit 
+//       state.status = "pending"
+//     },
+//     [productsFetch.fulfilled]: (state, action) => {
+//       state.status = "success"
+//       state.items = action.payload
+//     },
+//     [productsFetch.rejected]: (state, action) => {
+//       state.status = "pending"
+//       state.error = action.payload;
+//     }
+//   }
+// })
+
+// export default productsSlice.reducer;
