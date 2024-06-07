7c97e03d8ce87a60cff63e11f158c193cd75285a
Vanessa Bou <rvbouu@gmail.com>
2024-06-06 19:46:17 -0500

    added new seed data to product

--- client/src/components/CheckoutForm.jsx
+++ client/src/components/CheckoutForm.jsx
@@ -4,7 +4,8 @@ import {
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { useCallback } from 'react';
-import { useAppContext } from "../providers/AppProvider"
+import { useAppContext } from "../providers/AppProvider";
+// import HomePage from '../pages/HomePage';
 
 const stripePromise = loadStripe("pk_test_51PO0m9P1dwZ6prVFf0Fd2XT9kB4g8koQLfRGZnI5MLhUG55JGOV5sqnkszw4OOWhI1HgD1JKwXkkMmRk1vPa7vSS003ziIE66r");
 
