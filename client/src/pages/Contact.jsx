ef6b2d19fa891320d7b2d6d0a9efe0bede7cd216
CodeChef <joetomsim@gmail.com>
2024-06-07 23:10:21 -0500

    contact form function

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -10,108 +10,159 @@ export default function Contact() {
     const [message, setMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
-    const handleInputChange = (e) => {
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
+    const [formData, setFormData] = useState({
+        firstName: "", lastName: "", email: "", message: ""
+    })
 
-        if (inputType === 'email') {
-            if (inputValue === '') {
-                setEmail('')
+    function clearForms() {
+        setFormData({
+            firstName: "", lastName: "", email: "", message: ""
+        })
+    }
+
+    function handleChange(event) {
+        setMessage("")
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleInputChange = (e) => {
+        console.log(e);
+        const { name, value } = e.target;
+        console.log(name, value);
+        setFormData({ ...formData, [name]: value });
+
+        if (name === 'email') {
+            if (value === '') {
+                setFormData('')
                 return setErrorMessage('Email is required')
             }
-            if (!regex.test(inputValue)) {
-                setEmail('')
+            if (!regex.test(value)) {
+                setFormData('')
                 return setErrorMessage('Please enter a valid email')
             } else {
                 setErrorMessage('')
             }
         }
-        if (inputType === 'firstName') {
-            if (inputValue === '') {
-                setFirstName('')
+        if (name === 'firstName') {
+            if (value === '') {
+                setFormData('')
                 return setErrorMessage('First name is required')
             }
         }
-        if (inputType === 'lastName') {
-            if (inputValue === '') {
-                setLastName('')
+        if (name === 'lastName') {
+            if (value === '') {
+                setFormData('')
                 return setErrorMessage('Last name is required')
             }
         }
-        if (inputType === 'message') {
-            if (inputValue === '') {
-                setMessage('')
+        if (name === 'message') {
+            if (value === '') {
+                setFormData('')
                 return setErrorMessage('Message is required')
             }
         }
     }
 
+    async function handleEmail(event) {
+
+        try {
+            const response = await fetch("/api/contact", {
+                method: 'POST',
+                body: JSON.stringify({
+                    firstName: formData.firstName,
+                    lastName: formData.lastName,
+                    email: formData.email,
+                    message: formData.message
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            console.log(response)
+            const result = await response.json()
+            if (result.status === "success") {
+                setErrorUpdateMessage("Update successful")
+            }
+            clearForms()
+        } catch (err) {
+            console.log(err)
+            setErrorUpdateMessage("We could not sign you up with the credentials provided")
+        }
+    }
+
     const form = useRef();
 
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs
-        .sendForm('service_c7aey73', 'template_j0wsdxo', form.current, {
-          publicKey: '6jmeWZE--gIJU4Au5',
-        })
-        .then(
-          () => {
-            console.log('SUCCESS!');
-          },
-          (error) => {
-            console.log('FAILED...', error.text);
-          },
-        );
-    };
+    function sendEmail(e) {
+        e.preventDefault();
 
+        emailjs
+            .sendForm('service_c7aey73', 'template_j0wsdxo', form.current, {
+                publicKey: '6jmeWZE--gIJU4Au5',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+                handleEmail()
+            );
+    };
 
     return (
         <>
             <div className="form-field">
                 <h3 className='contact-title'>I'd love to hear from you!</h3>
-                <form 
+                <form
                     className="contact-form"
                     ref={form}
                     onSubmit={sendEmail}>
                     <label className="firstName">First Name:</label>
                     <input
                         className='input'
-                        defaultValue={firstName}
+                        value={formData.firstName}
                         name="firstName"
                         onBlur={handleInputChange}
+                        onChange={handleChange}
                         type="text"
                         placeholder="Gary"
                     />
                     <label className="lastName">Last Name:</label>
                     <input
                         className='input'
-                        defaultValue={lastName}
+                        value={formData.lastName}
                         name="lastName"
                         onBlur={handleInputChange}
+                        onChange={handleChange}
                         type="text"
                         placeholder="Almes"
                     />
                     <label className="email">E-mail</label>
                     <input
                         className='input'
-                        defaultValue={email}
+                        value={formData.email}
                         name="email"
                         onBlur={handleInputChange}
+                        onChange={handleChange}
                         type="email"
                         placeholder="garytalmes@hotmail.com"
                     />
                     <label className="message">Message</label>
                     <textarea
                         className='input'
-                        defaultValue={message}
+                        value={formData.message}
                         name='message'
                         onBlur={handleInputChange}
+                        onChange={handleChange}
                         type='message'
                         placeholder='Say hello!'
                     />
-                    <input className="submitbtn" type="submit" value="Send" />
+                    <button type='button' onClick={sendEmail}>Submit</button>
+                    {/* <input className="submitbtn" type="submit" value="Send" /> */}
                     {errorMessage && (
                         <div>
                             <p className="error-text">{errorMessage}</p>
